03fb1387f1e63ec2d8389a6106526997ec99ee0b
Donghyeok (Henry) Lee <119444211+Donghyeoklee93@users.noreply.github.com>
2023-02-16 13:54:04 +0000

    Add files via upload

---
+++ Recursion2.java
@@ -0,0 +1,14 @@
+public class Recursion2 {
+    public static void main(String[] args) {
+        int result = func(4);
+        System.out.println(result);
+    }
+
+    public static int func(int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            return n + func(n - 1);
+        }
+    }
+}
